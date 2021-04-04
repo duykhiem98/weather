55d31b5ae33fe3699876bc94010b633c2654181a
Your Name <you@example.com>
2021-04-04 19:03:34 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import Mainscreen from './src/Screen/Mainscreen';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Mainscreen);
---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+export const IC_BIEN1=require('./icon/Hinhbien1.png')
+export const IC_SUB=require('./icon/Subtract.png')
+export const IC_MAY=require('./icon/Vector.png')
+export const IC_RED=require('./icon/Rectangle 1632.png')
+export const IC_MAYMU=require('./icon/maymu.png')
